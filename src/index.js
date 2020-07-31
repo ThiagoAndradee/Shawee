65df45e853f7ae8b4347c2852ef6ada9877943d0
HSSNPdS <hssnpds@gmail.com>
2020-07-31 20:55:19 -0300

    Usando node.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 const express = require('express')
 const server = express()
 
-server.use(express.static('public'))
+server.use(express.static("public"))
 
-server.get('/', req, res => {
-  return res.render(index.html)
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({extended: true}))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
 })
 
-server.listen(3000)
+server.get('/', (req, res) => {
+  return res.render("index.html")
+})
+
+server.get('/login', (req, res) => {
+  return res.render("login.html")
+})
+
+server.get('/cadastro', (req, res) => {
+  return res.render("register.html")
+})
+
+server.get('/home', (req, res) => {
+  return res.render("home.html")
+})
+
+server.listen(3001)
